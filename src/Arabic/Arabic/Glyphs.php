ba40080dc651306289faaccb245d81614f7a5957
Mohamed Oussama EL ABBASSI <oussreda@gmail.com>
2021-05-11 19:00:52 +0000

    we have a bug here i proposed a solution
    we have a bug here i proposed a solution

--- src/Arabic/Arabic/Glyphs.php
+++ src/Arabic/Arabic/Glyphs.php
@@ -278,9 +278,11 @@ class I18N_Arabic_Glyphs
                 || $prevChar == 'لإ' || $prevChar == 'ل') 
                 && (mb_strpos('آأإا', $crntChar) !== false)
             ) {
-                if (mb_strpos($this->_prevLink, $chars[$i - 2]) !== false) {
-                    $form++;
-                }
+                if (isset($chars[$i - 2])) {
+		            if (mb_strpos($this->_prevLink, $chars[$i - 2]) !== false) {
+			            $form++;
+		            }
+	            }
                 
                 if (mb_strpos($this->_vowel, $chars[$i - 1])) {
                     $output .= '&#x';
